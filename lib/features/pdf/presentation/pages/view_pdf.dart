2af9f568f7b08c281a671408ef22f3cf7f7d9303
Mehedi <me69oshan@gmail.com>
2024-05-08 18:44:26 +0600

    pdf view initial prep

--- lib/features/pdf/presentation/pages/view_pdf.dart
+++ lib/features/pdf/presentation/pages/view_pdf.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:internet_file/internet_file.dart';
 import 'package:pdf_library/features/pdf/presentation/widgets/custom_back_button.dart';
+import 'package:pdfx/pdfx.dart';
 
-class ViewPdf extends StatelessWidget {
+class ViewPdf extends StatefulWidget {
   const ViewPdf({super.key});
+
+  @override
+  State<ViewPdf> createState() => _ViewPdfState();
+}
+
+class _ViewPdfState extends State<ViewPdf> {
+  late PdfController pdfController;
+  @override
+  void initState() {
+    pdfController = PdfController(
+      document: PdfDocument.openData(InternetFile.get(
+          'https://ia601608.us.archive.org/14/items/pdfy-vJFcaYG7Xau9y5ZZ/01_The_Lightning_Thief.pdf')),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,5 +36,9 @@ class ViewPdf extends StatelessWidget {
     );
   }
 
-  Widget _buildBody() => Container();
+  Widget _buildBody() {
+    return PdfView(
+      controller: pdfController,
+    );
+  }
 }
