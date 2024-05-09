f03f8266d88cf8f03a84e37fdf8e0236a823d7b0
Mehedi <me69oshan@gmail.com>
2024-05-09 14:29:19 +0600

    more error handle

--- lib/features/pdf/data/data_sources/remote/pdf_url_service.dart
+++ lib/features/pdf/data/data_sources/remote/pdf_url_service.dart
@@ -5,7 +5,8 @@ import 'package:pdf_library/features/pdf/data/models/pdf_model.dart';
 
 class PdfUrlService {
   Future<PdfModel> getRemotePdf(String url) async {
-    Uint8List data = await InternetFile.get(url);
+    Uint8List data = await InternetFile.get(url)
+        .onError((error, stacktrace) => throw Exception([error, stacktrace]));
     return PdfModel(data: data);
   }
 }
